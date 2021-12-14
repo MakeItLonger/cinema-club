fd2c99b5fd375b8bc75e0677d414ab1405292fdb
MakeItLonger <zavadiuk@gmail.com>
2021-12-14 16:55:54 +0200

    feature add search and trailers

--- js/services.js
+++ js/services.js
@@ -2,6 +2,9 @@ const API_KEY = '429274881df616664982155d8e559263';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const LANGUAGE = '&language=ru-RU';
 
+//https://api.themoviedb.org/3/tv/{tv_id}/videos?api_key=<<api_key>>&language=en-US
+//https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US
+
 
 const getData = url => {
     return fetch(url)
@@ -28,3 +31,15 @@ export const getPopular = async (type, page = 1) => {
     const url = `${BASE_URL}${type}/popular?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
     return await getData(url);
 };
+
+export const getVideo = async (id, type) => {
+    const url = `${BASE_URL}${type}/${id}/videos?api_key=${API_KEY}${LANGUAGE}`;
+    return await getData(url);
+}
+
+//https://api.themoviedb.org/3/search/multi?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
+
+export const search = async (query, page) => {
+    const url = `${BASE_URL}search/multi?api_key=${API_KEY}${LANGUAGE}&page=${page}&include_adult=false&query=${query}`;
+    return await getData(url);
+}
