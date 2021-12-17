00c516a64cffe5a10e99f9144164b1e5c924d95a
MakeItLonger <zavadiuk@gmail.com>
2021-12-17 17:14:52 +0200

    fix remove listener scroll after searching

--- js/search.js
+++ js/search.js
@@ -1,5 +1,6 @@
-import { search as getSearch } from "./services.js";
-import renderCard from "./renderCard.js";
+import { search as getSearch } from './services.js';
+import renderCard from './renderCard.js';
+import { addNewPage } from './scroll.js';
 
 const title = document.querySelector('.other-films__title');
 const filmWeek = document.querySelector('.film-week');
@@ -8,28 +9,29 @@ const searchInput = document.querySelector('.header__search-input');
 const listCard = document.querySelector('.other-films__list');
 
 const search = () => {
-    searchForm.addEventListener('submit', (e) => {
-        e.preventDefault();
+  searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.removeEventListener('scroll', addNewPage);
 
-        if (!searchInput.value) return;
+    if (!searchInput.value) return;
 
-        getSearch(searchInput.value)
-            .then(data => {
-                if (data.results.length) {
-                    renderCard(data.results);
-                } else {
-                    listCard.textContent = '';
-                    throw 'К сожалению по вашему запросу ничего не найдено'
-                }
-            })
-            .then(() => {
-                filmWeek.remove();
-                title.textContent = 'Результат поиска';
-            })
-            .catch(err => {
-                title.textContent = err;
-            })
-    })
+    getSearch(searchInput.value)
+      .then((data) => {
+        if (data.results.length) {
+          renderCard(data.results);
+        } else {
+          listCard.textContent = '';
+          throw 'К сожалению по вашему запросу ничего не найдено';
+        }
+      })
+      .then(() => {
+        filmWeek.remove();
+        title.textContent = 'Результат поиска';
+      })
+      .catch((err) => {
+        title.textContent = err;
+      });
+  });
 };
 
 export default search;
