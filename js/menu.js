65e96cedf656fbf79e8226cc1d50c5cb3d72f674
MakeItLonger <zavadiuk@gmail.com>
2021-12-20 16:03:15 +0200

    feature add sticky menu

--- js/menu.js
+++ js/menu.js
@@ -1,44 +1,33 @@
+const slideMenu = ({ openBtn, menu, classActiveMenu, closeTrigger }) => {
+  const burgerBtn = document.querySelector(openBtn);
+  const navigation = document.querySelector(menu);
+  const navigationClose = document.querySelectorAll(closeTrigger);
 
+  const closeMenu = (e) => {
+    if (e._nav || (e.target.closest(menu) && !e.target.closest(closeTrigger))) {
+      return;
+    }
 
-const slideMenu = ({
-    openBtn,
-    menu,
-    classActiveMenu,
-    closeTrigger
-}) => {
+    if (e.type === 'keydown' && e.code !== 'Escape') {
+      return;
+    }
 
+    navigation.classList.remove(classActiveMenu);
+    document.removeEventListener('click', closeMenu);
+    document.removeEventListener('keydown', closeMenu);
+  };
 
-    const burgerBtn = document.querySelector(openBtn);
-    const navigation = document.querySelector(menu);
-    const navigationClose = document.querySelectorAll(closeTrigger);
-    
-    const closeMenu = e => {
-        
+  burgerBtn.addEventListener('click', (e) => {
+    navigation.classList.add(classActiveMenu);
+    e._nav = true;
 
-        if (e._nav || (e.target.closest(menu) && !e.target.closest(closeTrigger))) {
-            return
-        }
+    document.addEventListener('click', closeMenu);
+    document.addEventListener('keydown', closeMenu);
+  });
 
-        if (e.type === 'keydown' && e.code !== 'Escape') {
-            return
-        }
-
-        navigation.classList.remove(classActiveMenu);
-        document.removeEventListener('click', closeMenu);
-        document.removeEventListener('keydown', closeMenu);
-    };
-
-    burgerBtn.addEventListener('click', e => {
-        navigation.classList.add(classActiveMenu);
-        e._nav = true;
-
-        document.addEventListener('click', closeMenu);
-        document.addEventListener('keydown', closeMenu);
-    });
-
-    navigationClose.forEach((item) => {
-        item.addEventListener('click', closeMenu)
-    });
+  navigationClose.forEach((item) => {
+    item.addEventListener('click', closeMenu);
+  });
 };
 
 export default slideMenu;
